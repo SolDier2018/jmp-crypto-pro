5d796521dd84e481aae39e74d291da781e4bd316
Eugeniy Manuylov <astels2009@gmail.com>
2024-06-21 19:14:12 +0700

    добавляет build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 export default defineConfig({
 
     build: {
+        outDir: 'build',
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'CryptoProSignature',
