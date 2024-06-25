7b9ca9ff60f326b9185e1d2db353b6286308ff69
Eugeniy Manuylov <astels2009@gmail.com>
2024-06-25 19:44:23 +0700

    изменяет импорт библиотеки

--- src/utils/after-plugin-loaded.ts
+++ src/utils/after-plugin-loaded.ts
@@ -3,17 +3,16 @@ type UnPromisify<T> = T extends Promise<infer R> ? R : T;
 let isSetLogLevel = false;
 let isPluginLoaded = false;
 
-const cadespluginApiUrl =
-    'https://www.cryptopro.ru/sites/default/files/products/cades/cadesplugin_api.js';
-
 export const afterPluginsLoaded = <T extends (...args: any[]) => any>(
     fn: T
 ): ((...args: Parameters<T>) => Promise<UnPromisify<ReturnType<T>>>) => {
     return async (...args: Parameters<T>): Promise<UnPromisify<ReturnType<T>>> => {
         if (!isPluginLoaded) {
             try {
-                require(cadespluginApiUrl);
+                await require('../vendor/cadesplugin_api');
             } catch (error) {
+                console.error(error);
+
                 throw new Error('Ошибка при подключении модуля для работы с Cades plugin');
             }
 
